d034067f4afbeaf914882e3cd06d9c041f9b1dd3
subut0n <a.dzikowski@outlook.fr>
2021-05-27 15:36:43 +0200

    jeudi cool 15h30

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 16:41:28 by addzikow          #+#    #+#             */
-/*   Updated: 2021/05/27 14:25:53 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2021/05/27 15:34:46 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,28 @@ typedef struct s_deque
 	int	size;
 }	t_deque;
 
-int check_limit(char *arg);
-int check_arg(char *arg);
-int	check_all_args(int ac, char **av);
+t_deque_list	*init_deque_list(void *ptr);
+t_deque			*init_deque(void);
+void			deque_add_front(t_deque *deque, void *ptr);
+void			deque_add_back(t_deque *deque, void *ptr);
+void			deque_pop_front(t_deque *deque, void (*f)(void *));
+void			deque_pop_back(t_deque *deque, void (*f)(void *));
+
+int 			check_limit(char *arg);
+int 			check_arg(char *arg);
+int				check_all_args(int ac, char **av);
+
+void			sa(t_deque *stack);
+void			sb(t_deque *stack);
+void			ss(t_deque *stack[2]);
+void			pa(t_deque *stack[2]);
+void			pb(t_deque *stack[2]);
+void			ra(t_deque *stack);
+void			rb(t_deque *stack);
+void			rr(t_deque *stack[2]);
+void			rra(t_deque *stack);
+void			rrb(t_deque *stack);
+void			rrr(t_deque *stack[2]);
+
 
 #endif
--- srcs/action.c
+++ srcs/action.c
@@ -6,10 +6,120 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 14:07:36 by addzikow          #+#    #+#             */
-/*   Updated: 2021/05/27 14:12:20 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2021/05/27 15:29:19 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/push_swap.h"
 
+void sa(t_deque *stack)
+{
+	void *content[2];
 
-void sa(t_de)
+	content[0] = stack->head->content;
+	deque_pop_front(stack,NULL);
+	content[1] = stack->head->content;
+	deque_add_front(stack, content[0]);
+	deque_add_front(stack, content[1]);
+}
+
+void sb(t_deque *stack)
+{
+	void *content[2];
+
+	content[0] = stack->head->content;
+	deque_pop_front(stack,NULL);
+	content[1] = stack->head->content;
+	deque_add_front(stack, content[0]);
+	deque_add_front(stack, content[1]);
+}
+
+void ss(t_deque *stack[2])
+{
+	sa(stack[A]);
+	sb(stack[B]);
+}
+
+void pa(t_deque *stack[2])
+{
+	void *content;
+
+	content = stack[B]->head->content;
+	if (stack[B]->size > 0)
+	{
+		deque_pop_front(stack[B], NULL);
+		deque_add_front(stack[A], content);
+	}
+}
+
+void pb(t_deque *stack[2])
+{
+	void *content;
+
+	content = stack[A]->head->content;
+	if (stack[A]->size > 0)
+	{
+		deque_pop_front(stack[A], NULL);
+		deque_add_front(stack[B], content);
+	}
+}
+
+void ra(t_deque *stack)
+{
+	void *content;
+
+	content = stack->head->content;
+	if (stack->size > 1)
+	{
+		deque_pop_front(stack, NULL);
+		deque_add_back(stack, content);
+	}
+}
+
+void rb(t_deque *stack)
+{
+	void *content;
+
+	content = stack->head->content;
+	if (stack->size > 1)
+	{
+		deque_pop_front(stack, NULL);
+		deque_add_back(stack, content);
+	}
+}
+
+void rr(t_deque *stack[2])
+{
+	ra(stack[A]);
+	rb(stack[B]);
+}
+
+void rra(t_deque *stack)
+{
+	void *content;
+
+	content = stack->tail->content;
+	if (stack->size > 1)
+	{
+		deque_pop_back(stack, NULL);
+		deque_add_front(stack, content);
+	}
+}
+
+void rrb(t_deque *stack)
+{
+	void *content;
+
+	content = stack->tail->content;
+	if (stack->size > 1)
+	{
+		deque_pop_back(stack, NULL);
+		deque_add_front(stack, content);
+	}
+}
+
+void rrr(t_deque *stack[2])
+{
+	rra(stack[A]);
+	rrb(stack[B]);
+}
